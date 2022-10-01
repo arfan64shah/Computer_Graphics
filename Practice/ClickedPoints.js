a6baaf1a71e40481f6b1a6ac4d8ff754f9894f1c
arfan64shah <59228458+arfan64shah@users.noreply.github.com>
2022-10-01 21:44:16 +0600

    Updated clickedpoints by removing clear function

--- Practice/ClickedPoints.js
+++ Practice/ClickedPoints.js
@@ -56,7 +56,7 @@ function click(ev, gl, canvas, a_Position) {
   g_points.push(x); g_points.push(y);
 
   // Clear <canvas>
-  gl.clear(gl.COLOR_BUFFER_BIT);
+  //gl.clear(gl.COLOR_BUFFER_BIT);
 
   var len = g_points.length;
   for(var i = 0; i < len; i += 2) {
