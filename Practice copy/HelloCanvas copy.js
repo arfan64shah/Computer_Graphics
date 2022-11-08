f15b33c1a0ae61549a0eb453e105db555e2c46a9
Arfan Shah <arfan.shah_2023@ucentralasia.org>
2022-11-08 18:45:19 +0600

    Added js code for drawing a point

---
+++ Practice copy/HelloCanvas copy.js
@@ -0,0 +1,21 @@
+
+function main() {
+  // Retrieve <canvas> element
+  var canvas = document.getElementById('webgl');
+
+  // Get the rendering context for WebGL
+  var gl = getWebGLContext(canvas);
+  if (!gl) {
+    console.log('Failed to get the rendering context for WebGL');
+    return;
+  }
+
+  // Set clear color
+  gl.clearColor(0.0, 1.0, 0.0, 1.0);
+
+  // Clear <canvas>
+  gl.clear(gl.COLOR_BUFFER_BIT);
+
+  gl.drawColor(1.0, 0.0, 1.0, 1.0);
+  gl.drawPoint(0.0, 0.0, 0.0, 10);
+}
