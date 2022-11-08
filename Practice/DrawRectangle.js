a20b2464c80b008af0f02f3745f6e4e82ae87395
Arfan Shah <arfan.shah_2023@ucentralasia.org>
2022-11-08 15:50:57 +0600

    Updated changes in draw rectangle

--- Practice/DrawRectangle.js
+++ Practice/DrawRectangle.js
@@ -1,4 +1,4 @@
-// DrawTriangle.js (c) 2012 matsuda
+
 function main() {  
   // Retrieve <canvas> element
   var canvas = document.getElementById('example');  
@@ -11,6 +11,6 @@ function main() {
   var ctx = canvas.getContext('2d');
 
   // Draw a blue rectangle
-  ctx.fillStyle = 'rgba(0, 0, 255, 1.0)'; // Set color to blue
+  ctx.fillStyle = 'rgba(0, 255, 0, 0.7)'; // Set color to blue
   ctx.fillRect(120, 10, 150, 150);        // Fill a rectangle with the color
 }
